32a8812081076e65f7ca8d0bf0ffbd21bc9ec410
LeVietThong <levietthong92@gmail.com>
2025-12-02 11:49:50 +0700

    fix ko run project

--- index.js
+++ index.js
@@ -2,35 +2,15 @@ const express = require("express");
 require("dotenv").config();
 const database = require("./config/database");
 
-const Task = require("./models/task.model");
-
 database.connect();
 
 const app = express();
 const port = process.env.PORT;
 
-const routesApiV1 = require("./v1/routes/index.route");
+const routesApiV1 = require("./api/v1/routes/index.route");
+
 
 routesApiV1(app);
-
-app.get("/tasks", async (req, res) => {
-  const task = await Task.find({
-    deleted: false,
-  });
-
-  res.json(task);
-});
-
-app.get("/tasks/detail/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const task = await Task.findOne({
-    _id: id,
-    deleted: false,
-  });
-
-  res.json(task);
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
