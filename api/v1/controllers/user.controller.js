46e1db1b8330d2fe42357da031fb4393d193543c
ThongLV <levietthong92@gmail.com>
2025-12-04 14:14:04 +0700

    register

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,17 +1,37 @@
+const e = require("cors");
 const User = require("../models/user.model");
 const md5 = require("md5");
 
 //[POST] api/v1/users/register
 module.exports.register = async (req, res) => {
-  // const existEmail = await User.findOne({ email: req.body.email });
+  req.body.password = md5(req.body.password);
 
-  // req.body.password = md5(req.body.password);
-
-  // const user = new User(req.body);
-  // await user.save();
-
-  res.json({
-    code: 200,
-    message: "Tạo user thành công",
+  const existEmail = await User.findOne({
+    email: req.body.email,
+    deleted: false
   });
+
+  if (existEmail) {
+    return res.json({
+      code: 400,
+      message: "Email đã tồn tại",
+    });
+  } else {
+    const user = new User({
+      fullName: req.body.fullName,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    await user.save();
+
+    const token = user.token;
+    res.cookie("token", token);
+
+    res.json({
+      code: 200,
+      message: "Tạo tài khoản thành công",
+      token: token
+    });
+  }
 };
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
-const database = require("./config/database");
+const database = require("./config/database"); 
 
 database.connect();
 
@@ -13,6 +14,7 @@ const routesApiV1 = require("./api/v1/routes/index.route");
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(cookieParser());
 
 routesApiV1(app);
 
